36aa1e582137ee655aa0d1c37c85d8e5dc68d90f
gav <opi9a@gavinroberts.com>
2017-06-01 17:39:29 +0100

    made functions, and enabled CLI options

--- cryptfolio.py
+++ cryptfolio.py
@@ -27,35 +27,55 @@ def get_prices(coins, datafile):
                 prices.append(float(entry['price_gbp']))
     return prices
 
-coins, vols = get_coins()
-prices = get_prices(coins, get_data())
-values = []
-total = 0.0
-
-for i,coin in enumerate(coins):
-    value=vols[i]*float(prices[i])
-    values.append(value)
-    total = total + value
-
-shares = []    
-for i,value in enumerate(values):
-    shares.append(value/total)
+def get_values(coins):  
+    values = []
+    total = 0.0
     
-len1 = len(max(coins, key=len))
-len2 = len(str(int(max(vols))))
-len3 = len(str(int(max(prices))))
-len4 = len(str(int(max(values))))
-pad = 2
+    for i,coin in enumerate(coins):
+        value=vols[i]*float(prices[i])
+        values.append(value)
+        total = total + value
+    
+    return values, total
 
-print("\nCOIN", " "*(len1+1), "PRICE        UNITS        VALUE       SHARE")
+def get_shares(coins):
+    shares = []    
+    for i,value in enumerate(values):
+        shares.append(value/total)
+    
+    return shares
 
-for i, coin in enumerate(coins):
-    print(coin, end = (" "*(len1+pad-len(coin))))
-    print("{:10,.2f}".format(prices[i]), " ",
-          "{:10,.2f}".format(vols[i]), " ",
-          "{:10,.0f}".format(values[i]),
-          "{:10,.1f}%".format(shares[i]*100))
+def print_folio(coins, vols, prices, values, shares, total):
+        
+    len1 = len(max(coins, key=len))
+    len2 = len(str(int(max(vols))))
+    len3 = len(str(int(max(prices))))
+    len4 = len(str(int(max(values))))
+    pad = 2
+    
+    print("\nCOIN", " "*(len1+1), "PRICE        UNITS        VALUE       SHARE")
+    
+    for i, coin in enumerate(coins):
+        print(coin, end = (" "*(len1+pad-len(coin))))
+        print("{:10,.2f}".format(prices[i]), " ",
+              "{:10,.2f}".format(vols[i]), " ",
+              "{:10,.0f}".format(values[i]),
+              "{:10,.1f}%".format(shares[i]*100))
+    
+    print("\nTOTAL", " "*37, "{:10,.0f}".format(total))
+    print("(non", coins[0], " "*30,  "{:10,.0f})".format(total-values[0]))
+    print("")
 
-print("\nTOTAL", " "*37, "{:10,.0f}".format(total))
-print("(non", coins[0], " "*30,  "{:10,.0f})".format(total-values[0]))
-print("")
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) == 2:
+        config_file = sys.argv[1]
+    else:
+        config_file = "config.txt"
+    coins, vols = get_coins(config_file)
+    prices = get_prices(coins, get_data())
+    values, total = get_values(coins)
+    shares = get_shares(coins)
+    print_folio(coins, vols, prices, values, shares, total)
+    
+    
