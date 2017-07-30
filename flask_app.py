6529860b7a4a4d10816aef9527f03a53826067b6
gav <opi9a@gavinroberts.com>
2017-07-30 13:28:50 +0100

    cleaning

--- flask_app.py
+++ flask_app.py
@@ -3,6 +3,8 @@ from flask_bootstrap import Bootstrap
 from cryptfolio import *
 import numpy as np
 import pandas as pd
+import os
+import time
 
 app = Flask(__name__)
 
@@ -14,19 +16,26 @@ def home():
 	out_dict, total =crypt_get("config.txt")
 
 	# plot shares: 
+	# if True:
+	# 	pass
 
 	names = np.array([x for x in out_dict])
 	values = np.array([out_dict[x][2] for x in out_dict])
 
+	try:
+		os.remove('static/pie.jpg')
+	except OSError:
+		pass
+
 	pie =  pd.Series(values, index=names)
+
 
 	pie.plot(kind='pie').get_figure().savefig('static/pie.jpg')
 
 	return render_template('table.html', 
 							out_dict=out_dict,
-							pie=pie,
 							total=total,
 							blockh=get_blockh())
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	app.run()
