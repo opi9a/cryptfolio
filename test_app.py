0ef2534b0166073a4b3ed8029a74adfd4c5fbc42
gav <opi9a@gavinroberts.com>
2017-10-23 14:35:23 +0100

    added nflsky

--- test_app.py
+++ test_app.py
@@ -4,20 +4,24 @@ from cryptfolio import *
 import numpy as np
 import pandas as pd
 from datetime import datetime
+import calendar
+import json
+
+import nflsky
 
 app = Flask(__name__)
-app.config['SECRET_KEY']="AJFPRG"
+app.config['SECRET_KEY']="YeCqhrYGgBqrwH5XRHuj4XFBmY"
 
 bootstrap = Bootstrap(app)
 
 @app.route('/')
 def home(conf="config.txt"):
 
-	t_base = session.setdefault('timestamp', 
-					datetime.now().strftime('%a %-d %b, %-H:%M:%S'))
-	t_now = datetime.now().strftime('%a %-d %b, %-H:%M:%S')
-	# need to make a useful data structure here. can't use df
-	# (NB can't use setdefault here, as default is still evaluated)
+	# get `basics` of portfolio (coins, vols, tickers) from session dict 
+	# if it exists, or create it (NB: *once*, that's the point) if it doesn't
+
+	# (NB can't use setdefault here, as default is still evaluated, 
+	# defeating the purpose - so just use if else)
 
 	if 'basics' in session.keys():
 		basics=session['basics']
@@ -26,9 +30,16 @@ def home(conf="config.txt"):
 		basics=get_basics(conf)#[ticks,vols]
 		session['basics']=basics
 
-	# Get 24h change, mktcap
+	# also get time portfolio was read from session dictionary.
+	# (don't care if default called as no resources needed)
+	t_base = session.setdefault('timestamp', 
+					datetime.now().strftime('%a %-d %b, %-H:%M:%S'))
 
-	# Build df
+	# and time now
+	t_now = datetime.now().strftime('%a %-d %b, %-H:%M:%S')
+
+
+	# Build df with all the info, starting with `basics`
 
 	df=pd.DataFrame(basics).set_index('coins')
 
@@ -49,16 +60,30 @@ def home(conf="config.txt"):
 	df['weight']=((df['values']/df['cap_gbp'])/btc_proportion)
 	df['£PPPW']=btc_proportion*df['cap_gbp']*0.01
 
+	temp_dict = {df.loc[i,'ticks']:df.loc[i,'values'] for i in df.index.values}
+
+	print(temp_dict)
+
 	return render_template('test_frame.html', 
 							df=df, 
 							total=total, total_ch=total_ch, total_perc_ch=total_perc_ch,
-							t_now=t_now)
+							temp_dict = json.dumps(temp_dict), t_now=t_now)
 
 @app.route('/reset/')
 def reset():
 	print("\nin reset\n")
 	session.clear()
 	return redirect(url_for('home'))
+
+@app.route('/nfl/')
+def nfl():
+	
+	start_day = datetime.now().weekday()
+	out = nflsky.get_shows(6)
+	
+	return render_template('nflsky.html', out=out, start_day=calendar.day_name[start_day])
+
+
 
 if __name__ == '__main__':
 	app.run()
